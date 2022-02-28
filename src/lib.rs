1258bb63e15f678d549742f4e6544fa35f4191da
Ndeta Innocent <you@example.com>
2022-02-28 17:31:03 +0200

    building the capacity and len of vec

--- src/lib.rs
+++ src/lib.rs
@@ -12,18 +12,29 @@ impl<T> Myvec<T> {
             capacity: 0,
         }
     }
+    pub fn push(&mut self) {
+        if self.capacity == 0 {}
+    }
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
 }
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let mut vec = Vec::new();
-        vec.push(1usize);
-        vec.push(2);
-        vec.push(3);
-        vec.push(4);
-        vec.push(5);
-        assert_eq!(vec.capacity(), 8);
-        assert_eq!(vec.len(), 5);
+        let mut vec: Myvec<usize> = Myvec::new();
+        // vec.push(1usize);
+        // vec.push(2);
+        // vec.push(3);
+        // vec.push(4);
+        // vec.push(5);
+        assert_eq!(vec.capacity(), 0);
+        assert_eq!(vec.len(), 0);
     }
 }
